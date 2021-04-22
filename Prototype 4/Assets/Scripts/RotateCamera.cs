108adeb280be62df074f85e69810877829f24ed1
Laam19 <akib19@student.sust.edu>
2021-04-22 11:44:28 +0600

    Created Player and Camera Possition

---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotatioSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horaizantalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horaizantalInput * Time.deltaTime * rotatioSpeed);
+    }
+}
