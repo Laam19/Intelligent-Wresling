f9a3823a8e2539ed66c50e13ba8ff79747a34a76
Laam19 <akib19@student.sust.edu>
2021-04-22 14:10:14 +0600

    Aded PowerUp And enemy spawner

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerUpIndicator;
+    private float powerStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPower = false;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -18,5 +21,33 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPower = true;
+            Destroy(other.gameObject);
+            powerUpIndicator.SetActive(true);
+            StartCoroutine(PowerUpCountDown());
+        }
+
+    }
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(7);
+        hasPower = false;
+        powerUpIndicator.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+        if (collision.gameObject.CompareTag("Enemy") && hasPower)
+        {
+            Debug.Log("Collided with" + collision.gameObject.name + "with the power set to" + hasPower);
+            enemyRb.AddForce(awayFromPlayer * powerStrength , ForceMode.Impulse);
+        }
     }
 }
---
+++ Prototype 4/Assets/Scripts/SpawnManazar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManazar : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange=9;
+    public int enemyCount;
+    void Start()
+    {
+        SpawnEnemyWave(3);
+    }
+
+    private void SpawnEnemyWave(int enemiesTOSpawn)
+    {
+        for(int i = 0; i < enemiesTOSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
+    }
+    private Vector3 GenerateSpawnPos()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosZ, 0, spawnPosZ);
+        return randomPos;
+    }
+}
